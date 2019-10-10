ab2a82796bee329bc93b21f8a748d9183e5144ee
Sergio Cifuentes <sergiocifuentes1485@gmail.com>
2019-10-10 15:46:08 -0600

    Lector De Archivo

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
 //imports
 
-const express = require ('express');
+const express = require('express');
 const app = express();
+const path = require('path');
 
 //rutas
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/perfil',(req,res)=>{
-    res.sendFile(__dirname+"/views/perfil.html");
+app.get('/perfil', (req, res) => {
+    res.sendFile(__dirname + "/views/perfil.html");
 });
-//configutacion
-app.set('view engine','ejs');
-app.set('views',__dirname+"/views");
 
-app.set('port',3000);
- 
+//middleware
+app.use(express.static(path.join(__dirname, 'public')));
+
+//configutacion
+app.set('view engine', 'ejs');
+app.set('views', __dirname + "/views");
+
+app.set('port', 3400);
+
 //escucha
-app.listen(app.get('port'),()=>{
-    console.log('escuchando',app.get('port'))
-console.log(__filename);
-})
+app.listen(app.get('port'), () => {
+    console.log('escuchando', app.get('port'))
+    console.log(__filename);
+});
--- src/public/cargador.js
+++ src/public/cargador.js
@@ -2,11 +2,25 @@ const getButton = document.querySelector("#buttonCargar");
 
 const getData = () => {
     axios.get(url).then(response => {
-            console.log(response.data);
-        })
+        console.log("hola");
+    })
         .catch(error => {
             console.log(error);
         });
 };
 
-getButton.addEventListener('click', getData);
+getButton.addEventListener('click', obtenerArchivo);
+
+function obtenerArchivo(files) {
+    var file = files[0];
+    var reader = new FileReader();
+    reader.onload = function(e) {
+      var output = document.getElementById("fileOutput");
+      output.textContent = e.target.result;
+      var string = output.textContent;
+      var o = string.split(/\s+\n*/);
+      var elementotxt = document.getElementById('txt');
+      elementotxt.value = o;
+    };
+    reader.readAsText(file);
+}
